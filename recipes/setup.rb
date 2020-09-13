9f9f9c096eea0f4ae89013d9b743b35a2b46f0c2
vagrant <vagrant@jestervm.cerner.com>
2020-09-13 05:23:10 +0000

    initial workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,19 @@
+package 'tree' do
+  action :install
+end
+
+#Default action will be taken
+package 'ntp'
+
+package 'git' do
+  action :install
+end
+
+file 'etc/motd' do
+  content 'This server is the property of TechnoTrainer'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+
